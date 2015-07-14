338a1c582912eab0d9d7eaf6114bad6d3e4465cb
Gareth Lowe <gareth.lowe@ao.com>
2015-07-14 17:07:33 +0100

    Changed timing to test deploy

--- src/game/pblander.ts
+++ src/game/pblander.ts
@@ -31,7 +31,7 @@ class SimpleGame {
         this.game.add
             .tween(logo.scale)
             .to({ x: 1, y: 1 },
-                10000,
+                1000,
                 Phaser.Easing.Bounce.Out,
                 true);
     }
