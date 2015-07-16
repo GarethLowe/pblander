9842aad331135fc21177a6bac2c8baea26b45995
Gareth Lowe <gizmostripe@gmail.com>
2015-07-16 21:18:07 +0100

    Test run of azure custom scripting deploys

---
+++ deploy.cmd
@@ -0,0 +1,33 @@
+@echo off
+CALL npm install typescript
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO Unable to install typescript
+	GOTO Error
+)
+
+
+IF (%1%=="") GOTO SetTsc
+cd %1%
+
+:SetTsc
+IF EXIST ".\node_modules\.bin\tsc.cmd" (
+    SET tsc=CALL ".\node_modules\.bin\tsc.cmd"
+    GOTO Build
+)
+IF EXIST "%ProgramFiles(x86)%\Microsoft Sdks\Typescript\0.9\tsc.exe" (
+    set tsc="%ProgramFiles(x86)%\Microsoft Sdks\Typescript\0.9\tsc.exe"
+    GOTO Build
+)
+IF EXIST "%ProgramFiles%\Microsoft Sdks\Typescript\0.9\tsc.exe" (
+    set tsc="%ProgramFiles%\Microsoft Sdks\Typescript\0.9\tsc.exe"  
+    goto Build
+GOTO
+ECHO TypeScript compiler not found
+EXIT 999
+
+:Build
+ECHO Building TypeScript: Payboard.Site.js (using %tsc%)
+%tsc% src\game\pblander.ts
+
+:Error
+EXIT %ERRORLEVEL%
